5f424444e5cd4d5df82aca466c7d12231e3048c7
Matt <matt.noone95@gmail.com>
2020-12-26 14:33:36 +1100

    Move InputMappings to engine layer and rewrite arrays as autoproperties

---
+++ Assets/Deft/Scripts/Input/InputMappings.cs
@@ -0,0 +1,42 @@
+﻿namespace Deft.Input
+{
+    public static class InputMappings
+    {
+        public static string[] KeyboardControlIDs { get; } = new string[]
+        {
+            "a",
+            "b",
+            "c",
+            "d",
+            "e",
+            "f",
+            "g",
+            "h",
+            "i",
+            "j",
+            "k",
+            "l",
+            "m",
+            "n",
+            "o",
+            "p",
+            "q",
+            "r",
+            "s",
+            "t",
+            "u",
+            "v",
+            "w",
+            "x",
+            "y",
+            "z"
+        };
+
+        public static string[] MouseControlIDs { get; } = new string[]
+        {
+            "leftButton",
+            "middleButton",
+            "rightButton"
+        };
+    }
+}
