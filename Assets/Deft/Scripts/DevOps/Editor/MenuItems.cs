82dad603c579393155416c33180cc13f24132560
Matt <matt.noone95@gmail.com>
2021-01-01 18:41:42 +1100

    Begin Deft DevOps namespace and add toggle Play Mode with F5

---
+++ Assets/Deft/Scripts/DevOps/Editor/MenuItems.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Deft.DevOps
+{
+    public static class MenuItems
+    {
+        [MenuItem("Deft/Toggle Play _F5")]
+        public static void TogglePlayMode()
+            => EditorApplication.ExecuteMenuItem("Edit/Play");
+    }
+}
+
