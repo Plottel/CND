501581ff317ff2ad4badf72dfc2676f6c6f23283
Cory Mifsud <cory.mifsud@gmail.com>
2021-02-10 23:01:04 +1100

    Copied in dev console

---
+++ Assets/DevConsole/DevConsole.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevConsole : MonoBehaviour
+{
+    public static DevConsole dc;
+    public GameObject scrollContent;
+    public GameObject MessagePrefab;
+
+    private List<GameObject> messages = new List<GameObject>();
+
+    void Start()
+    {
+        dc = this;
+    }
+
+    public void PostMessage(string message, GameObject sender = null, Time timeStamp = null)
+    {
+        GameObject messageInstance = Instantiate(MessagePrefab, scrollContent.transform);
+        var newMessageRect = messageInstance.GetComponent<RectTransform>();
+        float posY = -20;
+        float posX = newMessageRect.anchoredPosition.x;
+
+        if (messages.Count > 0)
+        {
+            var lastMessageRect = messages[messages.Count - 1].GetComponent<RectTransform>();
+            posY = lastMessageRect.anchoredPosition.y - 40;
+        }
+        newMessageRect.anchoredPosition = new Vector2(posX, posY);
+        messages.Add(messageInstance);
+        messageInstance.GetComponent<ConsoleMessage>().SetText(message);
+
+        var scrollRect = scrollContent.GetComponent<RectTransform>();
+        var size = new Vector2(scrollRect.sizeDelta.x, scrollRect.sizeDelta.y + newMessageRect.rect.height);
+        scrollRect.sizeDelta = size;
+    }
+
+    public void ClearMessages()
+    {
+        foreach (var messageInstance in messages)
+        {
+            Destroy(messageInstance);
+        }
+        messages.Clear();
+        var scrollRect = scrollContent.GetComponent<RectTransform>();
+        scrollRect.sizeDelta = new Vector2(scrollRect.sizeDelta.x, 0);
+    }
+}
---
+++ Assets/DevConsole/DragHandle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DragHandle : EventTrigger
+{
+    private bool dragging;
+    private RectTransform panel;
+    private RectTransform rect;
+
+    public void Start()
+    {
+        panel = transform.parent.parent.GetComponent<RectTransform>();
+        rect = GetComponent<RectTransform>();
+    }
+
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        DevConsole.dc.PostMessage("Start Drag");
+        dragging = true;
+    }
+
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        DevConsole.dc.PostMessage("End Drag");
+        dragging = false;
+    }
+
+    void Update()
+    {
+        if (dragging)
+        {
+            float xPos = Input.mousePosition.x - panel.sizeDelta.x / 2 + rect.sizeDelta.x / 2;
+            float yPos = Input.mousePosition.y - panel.sizeDelta.y / 2 + rect.sizeDelta.y / 2;
+            panel.anchoredPosition = new Vector2(xPos, yPos);
+        }
+    }
+}
